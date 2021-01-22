a532f020018f51d454316a28495fd05e883cf4eb
Chris Nesbitt-Smith <chris@cns.me.uk>
2021-01-22 01:01:49 +0000

    formatter

---
+++ src/formatter.ts
@@ -0,0 +1,51 @@
+import { GetResponseDataTypeFromEndpointMethod } from '@octokit/types'
+
+type PullRequestGetResponseDataType = GetResponseDataTypeFromEndpointMethod<
+  typeof octokit.pulls.get
+>
+
+interface RecordPullRequest {
+  repo: string
+  id: number
+  owner: string
+  author: string
+  created_at: number
+  closed_at: number
+  merged_at: number
+  assignees: number
+  reviewers: number
+  body_length: number
+  time_to_merge: number
+  merged_by: string
+  commits: number
+  review_comments: number
+  additions: number
+  deletions: number
+  changed_files: number
+}
+
+export function formatPullRequest (
+  pull: PullRequestGetResponseDataType
+): RecordPullRequest {
+  return {
+    repo: pull.base.repo.name,
+    id: pull.number,
+    owner: pull.base.user.login,
+    author: pull.user.login,
+    commits: pull.commits,
+    review_comments: pull.review_comments,
+    additions: pull.additions,
+    deletions: pull.deletions,
+    changed_files: pull.changed_files,
+    merged_by: pull.merged_by ? pull.merged_by.login : '',
+    created_at: Date.parse(pull.created_at),
+    closed_at: pull.closed_at ? Date.parse(pull.closed_at) : 0,
+    merged_at: pull.merged_at ? Date.parse(pull.merged_at) : 0,
+    assignees: pull.assignees ? pull.assignees.length : 0,
+    reviewers: pull.requested_reviewers ? pull.requested_reviewers.length : 0,
+    body_length: pull.body ? pull.body.length : 0,
+    time_to_merge: pull.merged_at
+      ? Date.parse(pull.merged_at) - Date.parse(pull.created_at)
+      : 0
+  }
+}
---
+++ tests/formatter.spec.ts
@@ -0,0 +1,91 @@
+import * as mod from '../src/formatter'
+import { expect, use } from 'chai'
+const pullGood = require('./fixtures/pull-good.json')
+const pullBad = require('./fixtures/pull-bad.json')
+use(require('chai-as-promised'))
+
+import 'mocha'
+
+describe('query a repository', () => {
+  // it('should return hello world', () => {
+  //   const result = hello('alphagov', 'govuk-prototype-kit')
+  //   return expect(result).to.eventually.equal({ aa: 'Affa' })
+  // })
+  it('should queue collecting all issues')
+  it('should queue collecting all pull requests')
+  it('should format a bad pull request', () =>
+    expect(mod.formatPullRequest(pullBad)).to.eql({
+      assignees: 0,
+      author: 'palnabarun',
+      body_length: 0,
+      closed_at: 0,
+      created_at: 1610130783000,
+      id: 1513,
+      merged_at: 0,
+      merged_by: '',
+      owner: 'kubernetes',
+      repo: 'k8s.io',
+      reviewers: 0,
+      time_to_merge: 0,
+      commits: 1,
+      review_comments: 0,
+      additions: 10,
+      deletions: 5,
+      changed_files: 2
+    }))
+
+  it('should format a good pull request', () =>
+    expect(mod.formatPullRequest(pullGood)).to.eql({
+      assignees: 9,
+      author: 'palnabarun',
+      body_length: 336,
+      closed_at: 1610132191000,
+      created_at: 1610130783000,
+      id: 1513,
+      merged_at: 1610132190000,
+      merged_by: 'k8s-ci-robot',
+      owner: 'kubernetes',
+      repo: 'k8s.io',
+      reviewers: 2,
+      time_to_merge: 1407000,
+      commits: 1,
+      review_comments: 0,
+      additions: 10,
+      deletions: 5,
+      changed_files: 2
+    }))
+
+  it('should format a status checks', () =>
+    expect(mod.formatPullRequest(pullGood)).to.eql({
+      assignees: 9,
+      author: 'palnabarun',
+      body_length: 336,
+      closed_at: 1610132191000,
+      created_at: 1610130783000,
+      id: 1513,
+      merged_at: 1610132190000,
+      merged_by: 'k8s-ci-robot',
+      owner: 'kubernetes',
+      repo: 'k8s.io',
+      reviewers: 2,
+      time_to_merge: 1407000,
+      commits: 1,
+      review_comments: 0,
+      additions: 10,
+      deletions: 5,
+      changed_files: 2
+    }))
+})
+
+describe('query a pull request', () => {
+  it('should write the pull request to the dynamodb table')
+  it('should queue retrieving the corresponding issue')
+  it('should queue retrieving the corresponding diff')
+  it('should queue retrieving the corresponding commits')
+  it('should queue retrieving the corresponding reviews')
+  it('should queue retrieving the corresponding review_comments')
+
+  describe('query an issue from pull request issue', () => {
+    it('should write the issue to the dynamodb table')
+  })
+})
