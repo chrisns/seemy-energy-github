9cc978c4cf1b14cab37679def6536b53b5824920
Chris Nesbitt-Smith <chris@cns.me.uk>
2021-01-25 21:37:35 +0000

    ditch mocha+chai etc for jest

---
+++ jest.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  moduleFileExtensions: [
+    'ts',
+    'tsx',
+    'js',
+    'jsx',
+    'json',
+    'node',
+  ],
+  coverageReporters: ['lcovonly', 'text'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    '**/*.ts',
+    '**/*.js',
+    '!.**',
+    '!tests/**',
+    '!node_modules/**',
+    '!coverage/**',
+    '!jest.config.js',
+    '!wallaby.js',
+  ]
+}
