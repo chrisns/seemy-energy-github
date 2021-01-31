e2cbbee46447b0cd61665286b76d68cd4ad17a8a
Chris Nesbitt-Smith <chris@cns.me.uk>
2021-01-31 00:34:22 +0000

    ETL a single pull request

--- wallaby.js
+++ wallaby.js
@@ -1,15 +1,20 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 module.exports = function (w) {
   return {
-    files: ['*.ts', 'tests/**/*.json', 'src/**/*.ts'],
+    files: ['*.ts', 'tests/**/*.json', 'src/**/*.ts', 'tests/jest-dynalite-config.js'],
     tests: ['tests/**/*.spec.ts'],
     env: {
       type: 'node',
     },
     testFramework: 'jest',
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     setup: function (w) {
       // eslint-disable-next-line @typescript-eslint/no-var-requires
-      require('dotenv').config();
+      require('dotenv').config()
+    },
+    workers: {
+      initial: 1,
+      regular: 1
     },
   }
 }
