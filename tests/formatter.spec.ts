54283485f8b96470e3ad4e4f3ab568db7a1bd4fc
Chris Nesbitt-Smith <chris@cns.me.uk>
2021-02-01 22:55:13 +0000

    simplify tests

--- tests/formatter.spec.ts
+++ tests/formatter.spec.ts
@@ -1,20 +1,20 @@
 import * as mod from '../src/formatter'
-import issueBadRaw from './fixtures/issue-bad.json'
-import issueGoodRaw from './fixtures/issue-good.json'
-import pullGoodRaw from './fixtures/pull-good.json'
-import pullBadRaw from './fixtures/pull-bad.json'
-const issueBad = JSON.parse(JSON.stringify(issueBadRaw))
-const issueGood = JSON.parse(JSON.stringify(issueGoodRaw))
-const pullBad = JSON.parse(JSON.stringify(pullBadRaw))
-const pullGood = JSON.parse(JSON.stringify(pullGoodRaw))
+import issueBad from './fixtures/issue-bad.json'
+import issueGood from './fixtures/issue-good.json'
+import pullGood from './fixtures/pull-good.json'
+import pullBad from './fixtures/pull-bad.json'
 
 describe('formatter', () => {
   describe('pull request', () => {
-    test('should format a bad pull request', () => expect(mod.formatPullRequest(pullBad)).toMatchSnapshot())
-    test('should format a good pull request', () => expect(mod.formatPullRequest(pullGood)).toMatchSnapshot())
+    test('should format a bad pull request', () =>
+      expect(mod.formatPullRequest(JSON.parse(JSON.stringify(pullBad)))).toMatchSnapshot())
+    test('should format a good pull request', () =>
+      expect(mod.formatPullRequest(JSON.parse(JSON.stringify(pullGood)))).toMatchSnapshot())
   })
   describe('issue', () => {
-    test('should format a good issue', () => expect(mod.formatIssue(issueGood)).toMatchSnapshot())
-    test('should format a bad issue', () => expect(mod.formatIssue(issueBad)).toMatchSnapshot())
+    test('should format a good issue', () =>
+      expect(mod.formatIssue(JSON.parse(JSON.stringify(issueGood)))).toMatchSnapshot())
+    test('should format a bad issue', () =>
+      expect(mod.formatIssue(JSON.parse(JSON.stringify(issueBad)))).toMatchSnapshot())
   })
 })
